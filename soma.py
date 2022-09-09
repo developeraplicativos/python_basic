0372035a0c264acf72b43cfbf6937fb9ccd1fb23
developeraplicativos <developer.aplicativos@gmail.com>
2022-09-09 00:51:40 -0300

    agenda com exportacao e importacao

--- soma.py
+++ soma.py
@@ -1,28 +1,48 @@
 AGENDA = {};
-AGENDA['Guilherme'] = {
-    'telefone':'819983233244',
-    'email':'discio@gmail.com',
-    'endereco':'rua nilson sabino pinnho, 322',
-}
-
-AGENDA['Maria'] = {
-    'telefone':'819983231111',
-    'email':'maria@gmail.com',
-    'endereco':'rua dois e tres, 322',
-}
+# AGENDA['Guilherme'] = {
+#     'telefone':'819983233244',
+#     'email':'discio@gmail.com',
+#     'endereco':'rua nilson sabino pinnho, 322',
+# }
+#
+# AGENDA['Maria'] = {
+#     'telefone':'819983231111',
+#     'email':'maria@gmail.com',
+#     'endereco':'rua dois e tres, 322',
+# }
 
 def mostrar_contato():
-    for contato in AGENDA:
-        buscar_contato(contato)
-        print('----------------------')
+    if AGENDA:
+        for contato in AGENDA:
+            buscar_contato(contato)
+            print('----------------------')
+    else:
+        print('agenda vazia')
 
 def buscar_contato(contato):
-    print('Nome: ',contato)
-    print('Telefone: ',AGENDA[contato]['telefone'])
-    print('Email: ',AGENDA[contato]['email'])
-    print('Endereço: ',AGENDA[contato]['endereco'])
+    try:
+        print('Nome: ', contato)
+        print('Telefone: ', AGENDA[contato]['telefone'])
+        print('Email: ', AGENDA[contato]['email'])
+        print('Endereço: ', AGENDA[contato]['endereco'])
+    except KeyError:
+        print('Contato inexistente ')
+    except Exception:
+        print('Algum erro aconteceu')
 
-def incluir_editar_contato(contato, telefone, email, endereco):
+def lerEntrada():
+    telefone = input('Digite o telefone: ')
+    email = input('Digite o email: ')
+    endereco = input('Digite o endereço: ')
+    return telefone,email,endereco
+
+def incluir_editar_contato(contato,telefone,email,endereco):
+    try:
+        AGENDA[contato]
+        print('editando : ', contato + '--------------------------')
+    except:
+        print('inserindo : ', contato + '--------------------------')
+
     AGENDA[contato] = {
         'telefone': telefone,
         'email': email,
@@ -31,8 +51,13 @@ def incluir_editar_contato(contato, telefone, email, endereco):
     print('>>> contato {} adcionado com sucesso'.format(contato))
 
 def excluir_contato(contato):
-    AGENDA.pop(contato)
-    print('>>> contato {} excluído com sucesso'.format(contato))
+    try:
+        AGENDA.pop(contato)
+        print('>>> contato {} excluído com sucesso'.format(contato))
+    except KeyError:
+        print('Contato inexistente')
+    except Exception:
+        print('Algum erro aconteceu')
 
 def menu():
     print('1 para incluir')
@@ -40,27 +65,59 @@ def menu():
     print('3 para excluit')
     print('4 para buscar')
     print('5 para listar')
+    print('6 para exportar')
+    print('7 para importar')
     print('0 para fechar')
 
+def exportCSV():
+    with open('agenda.csv', 'w') as file:
+        file.write('nome;telefone;email;endereco\n')
+        for contato in AGENDA:
+            telefone = AGENDA[contato]['telefone']
+            email = AGENDA[contato]['email']
+            endereco = AGENDA[contato]['endereco']
+            file.write("{};{};{};{}\n".format(contato,telefone,email,endereco))
+    print('agenda salva com sucesso')
 
-menu()
-opcao = input('escolha uma opção: ')
+def importarCSV(path):
+    try:
+        with open(path, 'r') as file:
+            linhas = file.readlines()
+            for linha in linhas:
+                detalhes = linha.strip().split(';')
+                contato = detalhes[0]
+                telefone = detalhes[1]
+                email = detalhes[2]
+                endereco = detalhes[3]
+                incluir_editar_contato(contato, telefone, email, endereco)
+    except Exception as error:
+        print('ocorreu um erro: ')
+        print(error)
 
-if opcao == '5':
-    mostrar_contato()
-elif opcao == '4':
-    buscar_contato(input('Digite o nome do contato:'))
-elif opcao == '1' or opcao == '2':
-    contato = input('Digite o nome: ')
-    telefone = input('Digite o telefone: ')
-    email = input('Digite o email: ')
-    endereco = input('Digite o endereço: ')
-    incluir_editar_contato(contato, telefone, email, endereco)
-    mostrar_contato()
-elif opcao == '3':
-    excluir_contato(input('Digite o nome do contato: '))
-elif opcao == '0':
-    print('Fechando programa')
-else:
-    print('Opção invalida')
 
+while True:
+    menu()
+    opcao = input('escolha uma opção: ')
+
+    if opcao == '5':
+        mostrar_contato()
+
+    elif opcao == '4':
+        buscar_contato(input('Digite o nome do contato:'))
+
+    elif opcao == '1' or opcao == '2':
+        contato = input('Digite o nome: ')
+        telefone,email,endereco = lerEntrada()
+        incluir_editar_contato(contato,telefone,email,endereco )
+    elif opcao == '7':
+        path = input('digite o path do arquivo: ')
+        importarCSV(path)
+    elif opcao == '6':
+        exportCSV()
+    elif opcao == '3':
+        excluir_contato(input('Digite o nome do contato: '))
+    elif opcao == '0':
+        print('Fechando programa')
+        break
+    else:
+        print('Opção invalida')
